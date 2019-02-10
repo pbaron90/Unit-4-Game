61f7f398feec2564ea61ffb931e3891421328221
pbaron <pebaron88@gmail.com>
2019-02-10 02:02:36 -0500

    progress

---
+++ assets/game.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+// Here we created an on-click event that responds to button clicks of the crystal image.
+
+$(".crystal-image").on("click", function() {
+
+  // Clicking the button triggers an alert message.
+  alert("You clicked a crystal!");
+
+});
+
+</script>
